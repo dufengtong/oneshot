04a7940f3d0f9053488d44c19287cad0454468af
Farah <dufengtong@gmail.com>
2025-02-25 07:31:59 -0500

    update

--- scripts/fullmodel_mouse_train_a100.py
+++ scripts/fullmodel_mouse_train_a100.py
@@ -6,10 +6,10 @@ NNs = [6636,6055,3575,4792,5804,2746, 4261]
 
 def main():
     mouse_id = 5
-    for mouse_id in [6]:
+    for mouse_id in [7,8,9]:
         # for nconv in [64]:
-        nconv1 = 192
-        nconv2 = 192
+        nconv1 = 16
+        nconv2 = 320
         seed = 1
         nlayers = 2
         n_max_neurons = NNs[mouse_id] # Total number of neurons
@@ -40,26 +40,26 @@ def main():
         # for nconv1 in nconv1_list:
         #     for nconv2 in nconv2_list:
         
-        for nlayers in range(1,5):
+        # for nlayers in range(1,5):
 
-        # Generate lists of neuron numbers and seed numbers using logarithmic spacing
-        # neuron_numbers = np.geomspace(1, 1000, num=10, dtype=int)
-        # neuron_numbers = np.unique(np.concatenate(([1], neuron_numbers)))  # Ensure 1 is included and remove duplicates
-        # seed_numbers = np.linspace(10, 1, num=len(neuron_numbers), dtype=int)
-        # for i, n_neuron in enumerate(neuron_numbers):
-        #     for seed in range(1, seed_numbers[i]+1):
+        # # Generate lists of neuron numbers and seed numbers using logarithmic spacing
+        # # neuron_numbers = np.geomspace(1, 1000, num=10, dtype=int)
+        # # neuron_numbers = np.unique(np.concatenate(([1], neuron_numbers)))  # Ensure 1 is included and remove duplicates
+        # # seed_numbers = np.linspace(10, 1, num=len(neuron_numbers), dtype=int)
+        # # for i, n_neuron in enumerate(neuron_numbers):
+        # #     for seed in range(1, seed_numbers[i]+1):
             
-                # stim_numbers = np.geomspace(500, 30000, num=10, dtype=int)
-                # stim_numbers = np.unique(stim_numbers)  # Remove duplicates that might occur due to rounding
-                # stim_numbers = [5000, 30000]
-                # for n_stim_train in stim_numbers:
-        # for hs_readout in hs_list:
-            if nlayers > 2:
-                    weight_decay_core += 0.1
-            prefix = f'fullmodel_{mouse_names[mouse_id]}_{nlayers}_{nconv1}_{nconv2}_seed{seed}'
-            bsub_cmd = f'bsub -n 2 -q gpu_a100 -gpu "num=1"  -J {prefix} -o {output_save_path}/{prefix}.out -e {output_save_path}/{prefix}.err "bash fullmodel_mouse_script_a100.sh {nlayers} {nconv1} {nconv2} {seed} {n_neuron} {n_stim_train} {weight_decay_core} {mouse_id} {conv1_ks} {conv2_ks} {pretrain_mouse_id} {hs_readout}"'
-            print(bsub_cmd)
-            os.system(bsub_cmd)
+        #         # stim_numbers = np.geomspace(500, 30000, num=10, dtype=int)
+        #         # stim_numbers = np.unique(stim_numbers)  # Remove duplicates that might occur due to rounding
+        #         # stim_numbers = [5000, 30000]
+        #         # for n_stim_train in stim_numbers:
+        # # for hs_readout in hs_list:
+        #     if nlayers > 2:
+        #             weight_decay_core += 0.1
+        prefix = f'fullmodel_{mouse_names[mouse_id]}_{nlayers}_{nconv1}_{nconv2}_seed{seed}'
+        bsub_cmd = f'bsub -n 2 -q gpu_a100 -gpu "num=1"  -J {prefix} -o {output_save_path}/{prefix}.out -e {output_save_path}/{prefix}.err "bash fullmodel_mouse_script_a100.sh {nlayers} {nconv1} {nconv2} {seed} {n_neuron} {n_stim_train} {weight_decay_core} {mouse_id} {conv1_ks} {conv2_ks} {pretrain_mouse_id} {hs_readout}"'
+        print(bsub_cmd)
+        os.system(bsub_cmd)
 
 if __name__=='__main__':
     main()
