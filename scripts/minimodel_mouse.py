53a9248929dc958af3e01f232ced95fc7b24a5d0
Farah <dufengtong@gmail.com>
2025-10-12 23:00:48 -0400

    update

--- scripts/minimodel_mouse.py
+++ scripts/minimodel_mouse.py
@@ -145,9 +145,9 @@ if args.pretrain_mouse_id > -100:
     elif args.pretrain_mouse_id == 8:
         pretrain_model_name = f'{pretrain_mouse_name}_{exp_date[args.pretrain_mouse_id]}_2layer_16_320_clamp_sensorium_depthsep_pool_nneurons_1655.pt'
     elif args.pretrain_mouse_id == 10:
-        pretrain_model_name = f'{pretrain_mouse_name}_{exp_date[args.pretrain_mouse_id]}_2layer_16_320_clamp_sensorium_depthsep_pool_nneurons_886.pt'
+        pretrain_model_name = f'{pretrain_mouse_name}_{exp_date[args.pretrain_mouse_id]}_2layer_16_320_clamp_sensorium_depthsep_pool_nocrop_nneurons_886.pt'
     elif args.pretrain_mouse_id == 11:
-        pretrain_model_name = f'{pretrain_mouse_name}_{exp_date[args.pretrain_mouse_id]}_2layer_16_320_clamp_sensorium_depthsep_pool_nneurons_306.pt'
+        pretrain_model_name = f'{pretrain_mouse_name}_{exp_date[args.pretrain_mouse_id]}_2layer_16_320_clamp_sensorium_depthsep_pool_nocrop_nneurons_306.pt'
     elif args.pretrain_mouse_id == 12:
         pretrain_model_name = f'{pretrain_mouse_name}_{exp_date[args.pretrain_mouse_id]}_2layer_16_320_clamp_sensorium_depthsep_pool_nneurons_1681.pt'
     pretrained_model_path = os.path.join(parent_dir, 'weights', 'fullmodel', mouse_names[args.pretrain_mouse_id], pretrain_model_name)
